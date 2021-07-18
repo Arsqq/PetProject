0151bf4d294f3ea36ebc75ae412f0fb76dbc6ace
Arsqq <arny723@gmail.com>
2021-07-18 14:44:26 +0300

    initial commit

---
+++ src/main/java/com/example/simplepet/SimplePetApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePetApplication.class, args);
+    }
+}
